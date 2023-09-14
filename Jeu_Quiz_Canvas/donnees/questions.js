cfeb3a9441d8fc38c423f531c98fca21bd686c5e
TsioryMihajanirina <120304504+TsioryMihajanirina@users.noreply.github.com>
2023-09-14 16:07:07 +0300

    Add files via upload
    Maj:
    -Feno ny questions am thème rehetra

--- Jeu_Quiz_Canvas/donnees/questions.js
+++ Jeu_Quiz_Canvas/donnees/questions.js
@@ -147,6 +147,77 @@ export const THEMES = {
             13:["Français","Anglais","Africain","Chinois"],
             14:["12h","01h","12h30","01h30"],
         }
+    },
+    histoire : {
+        questions : {
+            0 : "Où l\'homme devrait-il poser le pied avant la fin de la décennie, selon un discours de John Kennedy prononcé en 1961 ?",
+            1 : "Qui a été le premier Américain à voyager dans l\'espace ?",
+            2 : "De quel pays proviennent les astronautes que l\'on surnomme les \"taïkonautes\" depuis 1998",
+            3 : "Quel astronaute et vétéran américain a voyagé dans l\'espace à l\'âge de 77 ans ?",
+            4 : "Qui fut le premier astronaute britannique ?",
+            5 : "Qu\'a fait Youri Gagarine après être descendu de l\'autobus qui le menait à la navette spatiale lors de sa première mission dans l\'espace ?",
+            6 : "Qui a été le premier président catholique des Etats-Unis ?",
+            7 : "Qui a été le premier président des Etats-Unis né au 20ème siècle ?",
+            8 : "Quelle reine britannique a offert un bureau au gouvernement américain que personne n\'a utilisé jusqu\'à la présidence de John F.Kennedy ?",
+            9 : "Qui a été le premier président des Etats-Unis à tenir une conférence de presse télévisée ?",
+            10 : "Quel est le seul président des Etats-Unis à avoir offert à son frère un poste important dans son administration ?",
+            11 : "Quel sommet avait été conquis par 1919 personnes à la fin de 2003 ?",
+            12 : "Qui a répondu \"Parce qu\'il est là\" quand on lui a demandé pourquoi il voulait escalader le mont Everst ?",
+            13 : "Qui était le chef du groupe d\'alpinistes ayant escaladé avec succès l\'Everest en 1953 ?",
+            14 : "En quelle année l\'ISPM a été fondé ?"
+        },
+        reponses : {
+            0 : ["Sur la lune","Sur la planète Mars","Sur la planète Jupiter","Sur une météorite"],
+            1 : ["Alan Shepard","Edward Kenway","Duncan Walpole","Spohie Adenot"],
+            2 : ["De la Chine","De la Suisse","De France","Du Vietnam"],
+            3 : ["John Glenn","Daniel Burbank","Eileen Collins","McClain"],
+            4 : ["Helen Sharman","Timothy Peake","Hamish Harding","Michael Foale"],
+            5 : ["Il a uriné sur une roue de l\'autobus","Il est allé au bar","Il s\'est endormi dans sa baignoire","Il a regardé un film"],
+            6 : ["John F.Kennedy","Joe Biden","Bill Clinton","Jimmy Carter"],
+            7 : ["John F.Kennedy","Barack Obama","Gerald Ford","Donald Trump"],
+            8 : ["Reine Victoria","Édouard VII","Élisabeth II","Guillaume IV"],
+            9 : ["John F.Kennedy","Joe Biden","Barack Obama","Donald Trump"],
+            10 : ["John F.Kennedy","George W. Bush","Richard Nixon","Franklin D. Roosevelt"],
+            11 : ["Mont Everest","Mont Blanc","Mont Viso","Pointe Dufour"],
+            12 : ["George Mallory","Jordan Romero","Hillary","Norgay"],
+            13 : ["John Hunt","Edmund Hillary","Tenzing Norgay","Reinhold Messner"],
+            14 : ["1993","2003","1998","2012"]
+        }
+    },
+    sports : {
+        questions : {
+            0 : "Quelle équipe de baseball a remporté la série mondiale le plus grand nombre de fois ?",
+            1 : "Dans quelle ville jouait l\'équipe historique de la ligue majeure de baseball, aussi nommée les Trolley Dodgers ?",
+            2 : "Quel joueur des Yankees de New York a obtenu le plus grand nombre de présences officielles au bâton et de coups sûrs durant les séries mondiales ?",
+            3 : "Quelle ère du baseball, marquée par des scores faibles, s'est terminée par l\'interdiction de la balle mouillée et l\'adoption de la balle rapide ?",
+            4 : "Combien d\'équipes actuelles de la ligue majeure de baseball n\'ont jamais remporté la série mondiale ?",
+            5 : "Qui était le receveur du match lorsque Don Larsen a lancé la seule partie parfaite de l\'histoire de la série mondiale ?" ,
+            6 : "Quel buisson à fleurs épineux doit être habillé chaque printemps afin de produire de belles fleurs ?",
+            7 : "Qu\'ont en commun la kalmie à larges feuilles, la cléùatite du père Armand, la vigne commune et le jasmin-trompette ?",
+            8 : "Quelle peste a dévasté vos dahlias si vous y observez une traînée visqueuse ?",
+            9 : "Quel légume appelle-t-on parfois un zucchini ?",
+            10 : "De quel continent provient la tomate rouge ?",
+            11 : "Quel Etat américain produit le plus de pommes de terre après l\'Idaho ?",
+            12 : "Que doit attendre un lanceur de disque avant de sortir de la zone de lancer ?",
+            13 : "Quel quart-arrière des Steelers de Pittsburgh a établi un record national de lancer du javelot au collégial ?",
+            14 : "Quelle épreuve olympique possède deux styles: le lancer avec élan classique et le lancer avec élan en rotation ?"
+        },
+        reponses : {
+            0 : ["Les Yankees de New York"," Astros de Houston","Est. Baltimore","Ouest. Angels L.A. Angels"],
+            1 : ["Brooklyn","New York","Denver","Miami"],
+            2 : ["Yogi Berra","DJ LeMahieu","Aaron Judge","Giovanny Urshela"],
+            3 : ["L\'ère de la balle morte","l\'ère de la balle","l\'ère du bâton","l\ère du lanceur"],
+            4 : ["8","7","5","2"],
+            5 : ["Yogi Berra","Yadier Molina","Bengie Molina","José Molina"],
+            6 : ["Le Rosier","Les Pommiers d'ornement","Le Corylopsis","Les Magnolias"],
+            7 : ["Plantes grimpantes","Bignone","Campsis","azote"],
+            8 : ["Escargots","Limace","Papillon","Chenille"],
+            9 : ["Courgette","Tomate","Aricot Vert","Carotte"],
+            10 : ["L\'Amérique du Sud","Europe","Afrique","Asie"],
+            11 : ["Washington","Indiana","Philadelphie","San Francisco"],
+            12 : ["Il doit attendre que le disque retombe sur le terrain","Il doit attendre quelqu'un","Il ne doit jamais attendre","Il doit attendre le receveur"],
+            13 : ["Terry Bradshaw","Kenny Pickett","Mitchell Trubisky","Mason Rudolph"],
+            14 : ["Le lancer du poids","Lancer de javelot","lancer de disque","lancer de marteau"]
+        }
     }
-
 }
